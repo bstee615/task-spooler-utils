0888cbdd00281d9ddb5889c142de121b3914f2d0
Benjamin <benjis@iastate.edu>
2024-08-22 10:56:37 -0500

    Ask for confirmation

--- install.sh
+++ install.sh
@@ -7,9 +7,14 @@ set -e
 chmod +x bin/*
 
 # Add `bin` to your `$PATH`.
-if grep "# task-spooler-utils install" ~/.bashrc
-then
-    sed -i "s@.*# task-spooler-utils install.*@export PATH=\"\$PATH:$PWD/bin\" # task-spooler-utils install@1" ~/.bashrc
+read -p "This will update ~/.bashrc to add the task-spooler-utils to your PATH. Continue (y/n)? " CONT
+if [ "$CONT" = "y" ]; then
+    if grep "# task-spooler-utils install" ~/.bashrc >> /dev/null
+    then
+        sed -i "s@.*# task-spooler-utils install.*@export PATH=\"\$PATH:$PWD/bin\" # task-spooler-utils install@1" ~/.bashrc
+    else
+        echo "export PATH=\"\$PATH:$PWD/bin\" # task-spooler-utils install" >> ~/.bashrc
+    fi
 else
-    echo "export PATH=\"\$PATH:$PWD/bin\" # task-spooler-utils install" >> ~/.bashrc
+    echo "Canceled."
 fi
